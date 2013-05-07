c7c77d560d0dfb31cc49e06b481f159a671526e3
lookflying <lookflying@lookflying-ThinkPad-T420.(none)>
2013-05-07 11:41:04 +0800

    add argument to set 1280x1024

--- set_1280x1024.sh
+++ set_1280x1024.sh
@@ -1,4 +1,10 @@
 #/bin/bash
+if [ $# -eq 1 ]
+then
+	number=$1
+else
+	number=1
+fi
 xrandr --newmode `cvt 1280 1024 60|grep Modeline|awk '{$1="";print $0}'`
-xrandr --addmode VGA1 `cvt 1280 1024 60|grep Modeline|awk '{print $2}'`
-xrandr --output VGA1 --mode `cvt 1280 1024 60|grep Modeline|awk '{print $2}'`
+xrandr --addmode VGA$number `cvt 1280 1024 60|grep Modeline|awk '{print $2}'`
+xrandr --output VGA$number --mode `cvt 1280 1024 60|grep Modeline|awk '{print $2}'`
