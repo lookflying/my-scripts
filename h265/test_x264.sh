4dc08838970a097be6fb5ad5ad05781ba646e484
lookflying <lookflying@lookflying-desktop.(none)>
2013-07-11 15:25:57 +0800

    rm useless

--- h265/test_x264.sh
+++ h265/test_x264.sh
@@ -14,11 +14,8 @@ then
 elif [ $# -eq 6 ] && [ -f $1 ] 
 then
 	time x264 -o test.264 --profile high --level 3.1 --fps $4 --frames $5 --threads 1 --bitrate $6 $1 --input-res $2"x"$3
-elif [ $# -eq 1 ] && [ -f $1 ]
-then
-	time x264 -o test.264 --profile high --level 3.1 --fps 25 --frames 60 --threads 1 --bitrate 1000 $1 --input-res 1920x1080
 else
-	time x264 -o test.264 --profile high --level 3.1 --fps 25 --frames 60 --threads 1 --bitrate 1000 ./1080_blue_sky.yuv --input-res 1920x1080
+	echo usage: $0 yuv width height fps frames "[bitrate] [outputname]"
 fi
 
 if [ -f test.264 ]
