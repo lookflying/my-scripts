1f7660e9fa4d463ed35e6dfe29943d52b3a1d719
lookflying <lookflying@lookflying-desktop.(none)>
2013-07-11 14:37:31 +0800

    hevc use bps not kbps

--- h265/test_hevc.sh
+++ h265/test_hevc.sh
@@ -3,13 +3,17 @@ if [ $# -eq 8 ] && [ -f $1 ] && [ -f $2 ]
 then
 	cfg=${1##*/}
 	filename="$8_${cfg%.*}"
-	time /home/projects/h265/HM-11.0/bin/TAppEncoderStatic -c $1 -i $2 -wdt $3 -hgt $4 -fr $5 -f $6 --RateControl --TargetBitrate=$7 --BitstreamFile=$filename".bin" --ReconFile=$filename".yuv"
+	bitrate=`expr $7 \* 1000`
+	echo $7
+	time /home/projects/h265/HM-11.0/bin/TAppEncoderStatic -c $1 -i $2 -wdt $3 -hgt $4 -fr $5 -f $6 --RateControl --TargetBitrate=$bitrate --BitstreamFile=$filename".bin" --ReconFile=$filename".yuv"
 elif [ $# -eq 7 ] && [ -f $1 ] && [ -f $2 ]
 then
-	time /home/projects/h265/HM-11.0/bin/TAppEncoderStatic -c $1 --Profile=main --Level=6.2 -i $2 -wdt $3 -hgt $4 -fr $5 -f $6 --RateControl --TargetBitrate=$7
+	bitrate=`expr $7 \* 1000`
+	echo $7
+	time /home/projects/h265/HM-11.0/bin/TAppEncoderStatic -c $1 --Profile=main --Level=6.2 -i $2 -wdt $3 -hgt $4 -fr $5 -f $6 --RateControl --TargetBitrate=$bitrate
 elif [ $# -eq 6 ] && [ -f $1 ] && [ -f $2 ]
 then
-	time /home/projects/h265/HM-11.0/bin/TAppEncoderStatic -c $1 --Profile=main --Level=6.2 -i $2 -wdt $3 -hgt $4 -fr $5 -f $6 --RateControl --TargetBitrate=1000
+	time /home/projects/h265/HM-11.0/bin/TAppEncoderStatic -c $1 --Profile=main --Level=6.2 -i $2 -wdt $3 -hgt $4 -fr $5 -f $6 --RateControl --TargetBitrate=1000000
 else
 	echo usage: $0 cfg yuv width height fps frames "[bitrate] [filename]"
 fi
