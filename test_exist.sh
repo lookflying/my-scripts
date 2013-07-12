44f7990b8f0ad0a7c67bda2cbdf09fa5d33c6f51
lookflying <lookflying@lookflying-desktop.(none)>
2013-07-12 15:25:31 +0800

    test_exist enhancement

--- test_exist.sh
+++ test_exist.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+#set -x
+if [ $# -ge 2 ] && [ -d $1 ]
+then
+	dir=$1
+	shift
+fi
 names=""
 for p in $@
 do
 	names=$names$p"*"
 done
-
+if [ -n $dir ]
+then
+	names=$dir/$names
+fi
 for name in $names
 do
 	if [ -f $name ]
