919fc88ca6e33525d486c0b9dd9d55a9f5e09458
lookflying <lookflying@gmail.com>
2014-02-15 14:35:54 +0800

    add vtune cpu usage and bandwidth script

--- 265/show_throughput_logs.sh
+++ 265/show_throughput_logs.sh
@@ -5,11 +5,12 @@ then
 	for logdir in `ls $rootdir|grep tasks|sort -V`
 	do
 		./show_logs.sh $rootdir/$logdir | awk '
-		BEGIN{max=0;frames=0;}
+		BEGIN{max=0;frames=0;psnr_y=0;psnr_u=0;psnr_v=0}
 		{
 			if(max<$4){max=$4}
 			frame+=$3
+			if(psnr_y<$6){psnr_y=$6;psnr_u=$7;psnr_v=$8}
 		}
-		END{print $1"\t"frame"\t"max"\t"frame/max"\t"$6"\t"$7"\t"$8}'	
+		END{print $1"\t"frame"\t"max"\t"frame/max"\t"psnr_y"\t"psnr_u"\t"psnr_v}'	
 	done
 fi
---
+++ vtune_bandwidth.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+function report_usage_bandwidth(){
+	report_dir=$1
+	tmpfile=`mktemp`
+	amplxe-cl -R summary -r $report_dir 1>$tmpfile 2>/dev/null
+	elapsed_time=`cat $tmpfile|grep 'Elapsed Time:'|awk '{print $3}'`
+	cpu_count=`cat $tmpfile|grep 'Logical CPU Count'|awk '{print $4}'`
+	cpu_usage=`cat $tmpfile|grep 'CPU Usage:'|awk -v count=$cpu_count '{print $3/count}'`
+	memroy_bandwidth=`cat $tmpfile|grep 'UNC_IMC_DRAM_DATA'|awk -v t=$elapsed_time 'BEGIN{sum=0}{sum+=$2}END{print sum * 64 / 1000000000 / t}'`
+	rm -rf $tmpfile
+	echo -e $cpu_usage"\t"$memroy_bandwidth
+}
+
+if [ $# -eq 1 ]
+then
+	dir=$1
+	for result in `ls $dir|sort -V`
+	do
+		echo -n -e $result"\t"
+		report_usage_bandwidth $dir/$result
+	done
+fi
