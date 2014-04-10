15ccb682ebdaadb98687994e8d85630230b47382
lookflying <lookflying@gmail.com>
2014-04-10 07:53:46 +0000

    add extract_tar_gz.sh

---
+++ extract_tar_gz.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+if [ $# -eq 1 ]
+then
+	if [ -d $1 ]
+	then
+		count=0
+		for file in $1/* 
+		do
+#			echo $file
+			if [ -f $file ]
+			then
+				dir=${file%.tar.gz}
+				ext=${file#$dir.}
+				if [ $ext = "tar.gz" ]
+				then
+					count=$[$count + 1]
+					if [ -d $dir ]
+					then
+						echo "$dir exists, nothing changed"
+					else
+						if [ -f $dir ]
+						then
+							rm -rf $dir
+						fi
+						echo "make dir $dir"
+						mkdir $dir
+						cd $dir
+						echo "exact file $file"
+						tar xzf $file
+					fi
+				fi
+			fi
+		done
+		echo "$count processed"
+		exit 0
+	fi
+fi
+echo "usage: $0 dir"
