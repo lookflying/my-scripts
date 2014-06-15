4269e315e905b4adcbeea89f619aa9a49295ae53
lookflying <lookflying@gmail.com>
2014-06-15 13:06:57 +0800

    add path to mkdir

--- rt/test_switch.sh
+++ rt/test_switch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 testname=`date +%Y_%m_%d_%H_%M_%S`
-mkdir $testname
-cd $testname
+path=`basename $0`
+mkdir $pathi/$testname
+cd $path/$testname
 trace-cmd record -e "sched_switch" $@
 
