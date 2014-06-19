577ca85552ab97f68cd0b804ecbb4913fbb10702
lookflying <lookflying@gmail.com>
2014-06-19 21:54:09 +0800

    add deadline script for boot_guest

--- rt/test_switch_rt-app_name.sh
+++ rt/test_switch_rt-app_name.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -x
 testname=$1
 path=`dirname $0`
 logdir=$path/$testname
 mkdir $logdir
 cd $logdir
 shift
-trace-cmd record -e "sched_switch" rt-app -l $logdir $@
+trace-cmd record -e "sched_switch" rt-app -l . $@
 
