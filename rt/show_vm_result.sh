ff1b2e15e928bba7607a5ce139756f451d905e17
lookflying <lookflying@gmail.com>
2014-07-08 09:13:28 +0800

    show vm result according to number

--- rt/show_vm_result.sh
+++ rt/show_vm_result.sh
@@ -11,11 +11,30 @@ function latest_miss_rate()
 	fi
 }
 
-if [ $# -eq 1 ]
+function num_miss_rate()
+{
+		if [ $# -eq 2 ]
+		then
+			host_dir=$1
+			num=$2	
+			latest=`find $host_dir -mindepth 1 -maxdepth 1 -type d|sort -V|head -$num|tail -1`
+			echo $latest
+			$miss_rate $latest
+		fi
+}
+
+if [ $# -ge 1 ]
 then
 	dir=$1
 	for host_info in `find $dir -maxdepth 1 -mindepth 1 -type d|sort -V`
 	do
-		latest_miss_rate $host_info				
+		if [ $# -eq 1 ]
+		then
+			latest_miss_rate $host_info				
+		elif [ $# -eq 2 ]
+		then
+			num=$2
+			num_miss_rate $host_info $num
+		fi
 	done
 fi
