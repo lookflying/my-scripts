1e5a7b44622a0fed190f6228329e8052e5f2b1b8
lookflying <lookflying@gmail.com>
2014-07-19 23:50:18 +0800

    add pic_upload.py

--- web/multipart.py
+++ web/multipart.py
@@ -5,13 +5,13 @@ import urllib2
 import json
 
 #url = "http://pictrail.tk:8000/"
-url = "http://203.195.155.219:8000/"
+url = "http://203.195.155.219/"
 register_openers()
 data = {'hello': 'world', 'here': 'there',}
 json_data = json.dumps(data)
 
-datagen, headers = multipart_encode({"json": json_data})
-#datagen, headers = multipart_encode({"script1": open(__file__, "rb"), "json": json_data})
+#datagen, headers = multipart_encode({"json": json_data})
+datagen, headers = multipart_encode({"script1": open(__file__, "rb"), "json": json_data})
 #datagen, headers = multipart_encode({"script1": open(__file__, "rb")})
 
 request = urllib2.Request(url, datagen, headers)
