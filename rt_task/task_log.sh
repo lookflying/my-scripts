bb03bce3cb129e7a5c29068a310ec889c7e4eb7d
lookflying <lookflying@gmail.com>
2014-08-24 22:38:30 +0800

    now can check log with key

---
+++ rt_task/task_log.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+logname="run.log"
+keys="miss_cnt"
+if [ $# -ge 1 ]
+then
+	dir=$1
+	if [ -d $dir ]
+	then
+		if [ $# -ge 2 ]
+		then
+			shift
+			keys=$@
+		fi
+		logs=`find $dir -name $logname|sort -V`
+		if [ -n "$logs" ]
+		then
+			for log in $logs
+			do
+				name=${log%/$logname}
+				name=${name##*/}
+				echo -n -e  "$name\t"
+				for key in $keys
+				do
+					value=`grep $key $log`
+					if [ $? -eq 0 ]
+					then
+						pvalue=`echo $value|awk '{$1="";print $0}'`
+						echo -n -e "$pvalue\t"
+					fi
+				done
+				echo -n -e "\n"
+			done
+		fi
+	fi
+fi
