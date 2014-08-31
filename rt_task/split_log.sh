826dcbc2742f5a2118b437b3fe548e29c0f10dee
lookflying <lookflying@gmail.com>
2014-08-31 14:22:23 +0800

    now split support column names

--- rt_task/split_log.sh
+++ rt_task/split_log.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
-if [ $# -eq 3 ]
+if [ $# -ge 3 ]
 then
 	target_dir=$1
 	ref_columns=$2
 	columns=$3
+	names=$4
 	ref_columns=${ref_columns//","/" "}
+	names=`echo $names|sed 'y/,/\t/'`
 	mkdir -p $target_dir
 	rm -rf $target_dir/*
 	while read line
 	do
-	if [ -n "$line" ]
-	then
-	key=""
-		for col in $ref_columns
-		do
-			key=$key`echo $line|awk -v col=$col '{print $col"_"}'`
-		done
+		if [ -n "$line" ]
+		then
+			key=""
+			for col in $ref_columns
+			do
+				key=$key`echo $line|awk -v col=$col '{print $col"_"}'`
+			done
 			key=${key%_}
-#			echo $key
-#			echo $line |sed 'y/\t%#/   /'|tr -s " "|cut -d" " -f$columns --output-delimiter=" " >> $target_dir/$key".txt"
-			echo $line |sed 'y/%#/  /'|awk -v columns=$columns 'BEGIN{split(columns, cols, ",")}{for(i in cols){printf $cols[i]" "}printf "\n"}' >> $target_dir/$key".txt"
-	fi
+			if [ -n "$names" ] && [ ! -e $target_dir/$key".txt" ]
+			then
+				echo "$names" > $target_dir/$key".txt"
+			fi
+	#			echo $key
+	#			echo $line |sed 'y/\t%#/   /'|tr -s " "|cut -d" " -f$columns --output-delimiter=" " >> $target_dir/$key".txt"
+			echo $line |sed 'y/%#/  /'|awk -v columns=$columns 'BEGIN{split(columns, cols, ",")}{for(i in cols){printf $cols[i]"\t"}printf "\n"}' >> $target_dir/$key".txt"
+		fi
 	done
 else
-	echo "usage $0 target_dir ref_column1,ref_column2,ref_column3... col1,col2,col3..."
+	echo "usage $0 target_dir ref_column1,ref_column2,ref_column3,... col1,col2,col3,... [name1,name2,name3,...]"
 fi
