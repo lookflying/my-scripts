9acfc2022f5b6a67f5aaf87a77ad57590e0ab2f8
lookflying <lookflying@gmail.com>
2014-09-04 00:01:07 +0800

    add add_col.sh

---
+++ rt_task/analyze/add_col.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+set -x
+while getopts :o:c:n: opt
+do
+	case $opt in
+	o)
+		operation="$OPTARG"
+		;;
+	c)
+		columns="$OPTARG"
+		;;
+	n)
+		column_name="$OPTARG"
+		;;
+	esac
+done
+
+shift $[ $OPTIND - 1]
+
+if [ $# -ge 1 ]
+then
+	files=$@
+	column1=`echo $columns|cut -d, -f1`
+	column2=`echo $columns|cut -d, -f2`
+	for file in $files
+	do
+		case $operation in
+		min)
+			awk -v c1=$column1 -v c2=$column2 -v name=$column_name '
+				{
+					if(NR == 1 && $NF + 0 != $NF)
+					{
+						printf $0"\t"name"\n";
+						next;
+					}
+					value = ($c1 > $c2 ? $c2 : $c1);
+					printf $0"\t"value"\n"
+				}' $file |tee $file
+			;;
+		max)
+		awk -v c1=$column1 -v c2=$column2 -v name=$column_name '
+				{
+					if(NR == 1 && $NF + 0 != $NF)
+					{
+						printf $0"\t"name"\n";
+						next;
+					}
+					value = ($c1 > $c2 ? $c1 : $c2);
+					printf $0"\t"value"\n"
+				}' $file |tee $file
+			;;
+		add)
+		awk -v c1=$column1 -v c2=$column2 -v name=$column_name '
+				{
+					if(NR == 1 && $NF + 0 != $NF)
+					{
+						printf $0"\t"name"\n";
+						next;
+					}
+					value = $c1 + $c2;
+					printf $0"\t"value"\n"
+				}' $file |tee $file
+			;;
+		sub)
+		awk -v c1=$column1 -v c2=$column2 -v name=$column_name '
+				{
+					if(NR == 1 && $NF + 0 != $NF)
+					{
+						printf $0"\t"name"\n";
+						next;
+					}
+					value = $c1 - $c2;
+					printf $0"\t"value"\n"
+				}' $file |tee $file
+			;;
+		mul)
+		awk -v c1=$column1 -v c2=$column2 -v name=$column_name '
+				{
+					if(NR == 1 && $NF + 0 != $NF)
+					{
+						printf $0"\t"name"\n";
+						next;
+					}
+					value = $c1 * $c2;
+					printf $0"\t"value"\n"
+				}' $file |tee $file
+			;;
+		div)
+		awk -v c1=$column1 -v c2=$column2 -v name=$column_name '
+				{
+					if(NR == 1 && $NF + 0 != $NF)
+					{
+						printf $0"\t"name"\n";
+						next;
+					}
+					value = $c1 / $c2;
+					printf $0"\t"value"\n"
+				}' $file |tee $file
+			;;
+		esac
+	done
+else
+	echo "usage: $0 -o min|max|add|sub|mul|div -c <column1>,<column2> -n <column_name> <file1> <file2> ..."
+fi
+set +x
