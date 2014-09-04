ee8ac59e8ef563903c78a310f9b25b75a82074a0
lookflying <lookflying>
2014-09-04 23:10:36 +0800

    add header for last_before_first_line.sh

--- rt_task/analyze/last_before_first_line.sh
+++ rt_task/analyze/last_before_first_line.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
-while getopts :o:v:c:s: opt
-do
-	case $opt in
-	o)
-#		echo operation = $OPTARG
-		operation=$OPTARG
-		;;
-	v)
-#		echo value = $OPTARG
-		value=$OPTARG
-		;;
-	c)
-#		echo column = $OPTARG
-		column=$OPTARG
-		;;
-	s)
-		sort_column=$OPTARG
-		;;
-	esac
-done
-
-shift $[ $OPTIND - 1]
-
-if [ $# -ge 1 ] && [ -n "$column" ] && [ -n "$operation" ] && [ -n "$value" ] 
-then
+function last_line()
+{
 	logs=$@
 	for log in $logs
 	do
@@ -76,7 +53,56 @@ then
 		fi
 		echo
 	done
-else
+}
+
+while getopts :o:v:c:s: opt
+do
+	case $opt in
+	o)
+#		echo operation = $OPTARG
+		operation=$OPTARG
+		;;
+	v)
+#		echo value = $OPTARG
+		value=$OPTARG
+		;;
+	c)
+#		echo column = $OPTARG
+		column=$OPTARG
+		;;
+	s)
+		sort_column=$OPTARG
+		;;
+	esac
+done
+
+shift $[ $OPTIND - 1]
+
+if [ $# -ge 1 ] && [ -n "$column" ] && [ -n "$operation" ] && [ -n "$value" ] 
+then
+	logs=$@
+	header=""
+	for log in $logs
+	do
+		if [ -z "$header" ]
+		then
+			header=`head -1 $log`
+		else
+			firstline=`head -1 $log`
+			if [ "$header" != "$firstline" ]
+			then
+				header=""
+				break;
+			fi
+		fi
+	done
+	if [ -n "$header" ]
+	then
+		echo -e "file\t"$header
+	fi
+
+	last_line $logs|sort -V
+	else
 	echo "usage: $0 -c <column> -o <operation> -v <value> -s <sort_column> <logfile1> <logfile2> ..."
 	echo -e "\t-c\tcolumn"
 	echo -e "\t-o\toperation, support lt le eq ge gt"
