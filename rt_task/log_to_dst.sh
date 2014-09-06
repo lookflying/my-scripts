040dc878aeb66ad179dbdfa98781ee9d6d98fb2d
lookflying <lookflying>
2014-09-07 00:38:25 +0800

    log_to_dst use file content as name

--- rt_task/log_to_dst.sh
+++ rt_task/log_to_dst.sh
@@ -2,16 +2,17 @@
 if [ $# -ge 2 ]
 then
 	log_dst=$1
-	file_name=$2
+	file_name_line=$2
 	shift 2
 	cmd=$@
 	tmp_file=`mktemp`
 	$cmd |tee $tmp_file
+	file_name=`sed -n "$file_name_line"p $tmp_file`
 	rsync -av $tmp_file $log_dst/$file_name
 	if [ $? -ne 0 ]
 	then
 		echo "fail to send file($tmp_file)  to $log_dst/$file_name"
 	fi
 else
-	echo "usage: $0 <log_dst> <file_name> <cmd>..."
+	echo "usage: $0 <log_dst> <file_name_line> <cmd>..."
 fi
