c4190a30ad394697abec6f9486936f1554110bba
lookflying <lookflying>
2014-09-06 21:41:47 +0800

    add notify_check

---
+++ rt_task/notify_check.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+loop=0
+while getopts :p:l:a:u: opt
+do
+	case $opt in
+	p)
+		notify_path=$OPTARG
+		;;
+	l)
+		loop=$OPTARG
+		;;
+	u)
+		notify_user=$OPTARG
+		;;
+	a)
+		notify_ip=$OPTARG
+		;;
+	esac
+done
+shift $[ $OPTIND - 1 ]
+
+if [ $# -ge 1 ] && [ -n "$notify_user" ] && [ -n "$notify_ip" ] && [ -n "$notify_path" ]
+then
+	notify_keys=$@
+	running=1
+	begin=`date +%s`
+	while [ $running -eq 1 ]
+	do
+		running=0
+		for key in $notify_keys
+		do
+			state=`ssh $notify_user@$notify_ip "cat $notify_path/$key"`
+			if [ $? -ne 0 ]
+			then
+				running=1
+				break;
+			fi
+			if [ $state -ne 0 ]
+			then
+				running=1
+				break;
+			fi
+			continue
+		done
+		if [ $loop -ne 0 ]
+		then
+			sleep 1
+			continue
+		else
+			break
+		fi
+	done
+	if [ $loop -ne 0 ]
+	then
+		end=`date +%s`
+		echo last $[ $end - $begin ] sec
+	fi
+	exit $running
+else
+	echo "usage: $0 -l loop -u <notify_user> -a <notify_ip> -p <notify_path>  <notify_key1> <notify_key2> ..."
+fi
