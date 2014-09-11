f3df0f60bef4d9f489c85fb0f321dac0642f626f
lookflying <lookflying>
2014-09-12 00:32:16 +0800

    fix

--- rt_task/remote_trace.sh
+++ rt_task/remote_trace.sh
@@ -3,5 +3,5 @@ trace_dir=/dev/shm/host_trace
 cd /dev/shm
 mkdir -p $trace_dir
 cd $trace_dir
-ssh root@192.168.1.22 "trace-cmd record -e 'sched_wakeup*' -e sched_switch -e 'sched_migrate*' "
+trace-cmd record -e 'sched_wakeup*' -e sched_switch -e 'sched_migrate*'
 
