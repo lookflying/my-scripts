4991aa84eb0b1f02b27c3d36b1d64555ef6dc930
lookflying <lookflying@gmail.com>
2014-12-16 19:59:36 +0800

    rename

---
+++ rt_task/sync.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ips="192.168.1.59 192.168.1.60 192.168.1.61 192.168.1.62"
+function sync_script()
+{
+	ip=$1
+	echo sync scripts on $ip
+	ssh root@$ip "cd ~/my-scripts;git pull origin master"
+}
+function sync_utility()
+{
+	ip=$1
+	echo sync utility on $ip
+	ssh root@$ip "cd ~/my-utility;git pull origin master;make"
+}
+for ip in $ips
+do
+	sync_script $ip &
+	sync_utility $ip &
+done
+wait
+
+
--- rt_task/sync_scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ips="192.168.1.59 192.168.1.60 192.168.1.61 192.168.1.62"
-for ip in $ips
-do
-	echo sync scripts on $ip
-	ssh root@$ip "cd ~/my-scripts;git pull origin master"
-done
