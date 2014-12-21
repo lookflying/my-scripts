01fea270658eb2540619fba254ee74f7982233d7
lookflying <lookflying@gmail.com>
2014-12-21 15:49:04 +0800

    guest

--- rt_task/benchmark/guest.sh
+++ rt_task/benchmark/guest.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 ./trend.sh 10000000:5000000 30|tee /dev/shm/guest_100.log
-./trend.sh 8000000:4000000 30|tee /dev/shm/guest_80.log
 ./trend.sh 5000000:2500000 30|tee /dev/shm/guest_50.log
-./trend.sh 4000000:2000000 30|tee /dev/shm/guest_40.log
-./trend.sh 2000000:1000000 30|tee /dev/shm/guest_20.log
 ./trend.sh 1000000:500000 30|tee /dev/shm/guest_10.log
 cp /dev/shm/guest_*.log .
