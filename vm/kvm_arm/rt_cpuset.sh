156ae42e2386f9a424a8895291d9e59ba53436d0
lookflying <lookflying@gmail.com>
2014-12-24 04:30:36 +0800

    add rt_cpuset

---
+++ vm/kvm_arm/rt_cpuset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /dev/cpuset
+echo 1 > cpuset.cpu_exclusive
+echo 0 > cpuset.sched_load_balance
+#虚拟机cpuset，执行所有非VCPU操作
+mkdir -p vm
+echo 0 > vm/cpuset.cpus
+echo 0 > vm/cpuset.mems
+echo 1 > vm/cpuset.cpu_exclusive
+echo 0 > vm/cpuset.mem_exclusive
+#实时cpuset
+mkdir -p rt
+echo 1 > rt/cpuset.cpus
+echo 0 > rt/cpuset.mems 
+echo 1 > rt/cpuset.cpu_exclusive
+echo 0 > rt/cpuset.mem_exclusive
