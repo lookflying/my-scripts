0764eeb45b2e70944780c8f5ab02dec23b89e75d
lookflying <lookflying@gmail.com>
2015-01-25 01:08:47 +0800

    use ec2

--- chrome_socks5.sh
+++ chrome_socks5.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-google-chrome --proxy-server="socks5://127.0.0.1:9999" --host-resolver-rules="MAP * 0.0.0.0, EXCLUED 127.0.0.1"
+#google-chrome --proxy-server="socks5://127.0.0.1:9999" --host-resolver-rules="MAP * 0.0.0.0, EXCLUED 127.0.0.1"
+google-chrome --proxy-server="socks5://127.0.0.1:9998" --host-resolver-rules="MAP * 0.0.0.0, EXCLUED 127.0.0.1"
--- ec2_tunnel.sh
+++ ec2_tunnel.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh -D 0.0.0.0:9998 ec2-user@ec2.lookflying.com
+#ssh -D 0.0.0.0:9998 ec2-user@ec2.lookflying.com
+ssh -D 0.0.0.0:9998 ec2-user@singapore.lookflying.com
